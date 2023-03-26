1bbbcdd242a15f6d02a0815c4403dd90be8aad21
JsH <jsh1147@naver.com>
2023-03-27 05:11:35 +0900

    Initial commit

---
+++ src/main/java/hellogroup/helloname/HellonameApplication.java
@@ -0,0 +1,13 @@
+package hellogroup.helloname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellonameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellonameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hellogroup/helloname/HellonameApplicationTests.java
@@ -0,0 +1,13 @@
+package hellogroup.helloname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellonameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
